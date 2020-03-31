7105af005358bc2b8924195e15ba4ae9852edff2
espe-on <eljoe0@googlemail.com>
2020-03-31 13:38:28 +0100

    Implimented a distance finder interface and associated methods

---
+++ GasMon/DistanceFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GasMon
+{
+    interface IDistanceFinder
+    {
+        double VerticalDistance(Location firstLocation, Location secondLocation);
+        double HorizontalDistance(Location firstLocation, Location secondLocation);
+        double DiagonalDistance(Location firstLocation, Location secondLocation);
+    }
+
+    public class DistanceFinder : IDistanceFinder
+    {
+        public double VerticalDistance(Location firstLocation, Location secondLocation)
+        {
+            return Math.Abs(firstLocation.x - secondLocation.x);
+        }
+
+        public double HorizontalDistance(Location firstLocation, Location secondLocation)
+        {
+            return Math.Abs(firstLocation.y - secondLocation.y);
+        }
+
+        public double DiagonalDistance(Location firstLocation, Location secondLocation)
+        {
+            return
+                Math.Sqrt(
+                    Math.Pow(VerticalDistance(firstLocation, secondLocation), 2) +
+                    Math.Pow(HorizontalDistance(firstLocation, secondLocation), 2));
+        }
+    }
+}

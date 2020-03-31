b62f955ee140193f89c54444b7c7467ca3d7cfc8
espe-on <eljoe0@googlemail.com>
2020-03-31 11:55:46 +0100

    Brokeout Location fetcher which now returns a list of location

--- GasMon/LocationsFetcher.cs
+++ GasMon/LocationsFetcher.cs
@@ -1,7 +1,54 @@
-﻿namespace GasMon
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Newtonsoft.Json;
+
+namespace GasMon
 {
-    public class LocationsFetcher
+    interface ILocationFetcher
     {
-        
+        List<Location> LocationFetcher();
+    }
+
+    public class LocationsFetcher : ILocationFetcher
+    {
+        private static readonly string AccessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY");
+        private static readonly string SecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY");
+        private static readonly string BucketName = Environment.GetEnvironmentVariable("AWS_S3_BUCKET_NAME");
+        private const string KeyName = "locations.json";
+        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest2;
+        private static IAmazonS3 _client;
+
+        private static async Task<string> ReadObjectDataAsync(IAmazonS3 client)
+        {
+            GetObjectRequest request = new GetObjectRequest
+            {
+                BucketName = BucketName,
+                Key = KeyName
+            };
+            using GetObjectResponse response = await client.GetObjectAsync(request);
+            await using Stream responseStream = response.ResponseStream;
+            using StreamReader reader = new StreamReader(responseStream);
+            string
+                title = response.Metadata[
+                    "x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
+            string contentType = response.Headers["Content-Type"];
+            Console.WriteLine("Object metadata, Title: {0}", title);
+            Console.WriteLine("Content type: {0}", contentType);
+
+            var responseBody = reader.ReadToEnd(); // Now you process the response body.
+            return responseBody;
+        }
+
+        public List<Location> LocationFetcher()
+        {
+            _client = new AmazonS3Client(AccessKey, SecretKey, BucketRegion);
+            var returnedData = ReadObjectDataAsync(_client).Result;
+            return JsonConvert.DeserializeObject<List<Location>>(returnedData);
+        }
     }
 }
--- GasMon/Program.cs
+++ GasMon/Program.cs
@@ -1,55 +1,14 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Amazon;
-using Amazon.S3;
-using Amazon.S3.Model;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace GasMon
 {
     class Program
     {
-        private static readonly string AccessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY");
-        private static readonly string SecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY");
-        private static readonly string BucketName = Environment.GetEnvironmentVariable("AWS_S3_BUCKET_NAME");
-        private const string KeyName = "locations.json";
-        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest2;
-        private static IAmazonS3 _client;
-
         static void Main()
         {
-            _client = new AmazonS3Client(AccessKey,SecretKey,BucketRegion);
-            ReadObjectDataAsync().Wait();
+            Console.WriteLine("Hello World");
         }
-        static async Task ReadObjectDataAsync()
-        {
-            string responseBody = "";
-            try
-            {
-                GetObjectRequest request = new GetObjectRequest
-                {
-                    BucketName = BucketName,
-                    Key = KeyName
-                };
-                using GetObjectResponse response = await _client.GetObjectAsync(request);
-                await using Stream responseStream = response.ResponseStream;
-                using StreamReader reader = new StreamReader(responseStream);
-                string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
-                string contentType = response.Headers["Content-Type"];
-                Console.WriteLine("Object metadata, Title: {0}", title);
-                Console.WriteLine("Content type: {0}", contentType);
-
-                responseBody = reader.ReadToEnd(); // Now you process the response body.
-                Console.WriteLine(responseBody);
-            }
-            catch (AmazonS3Exception e)
-            {
-                Console.WriteLine("Error encountered ***. Message:'{0}' when writing an object", e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
-            }
-        }
+        
     }
 }

bf5c35b6570858c162747206cdab480b7f3ecbef
f <fdf>
2020-04-01 15:49:00 +0100

    class to create Readings from SNS messages

---
+++ GasMon/MessageParser.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.SQS.Model;
+using Newtonsoft.Json;
+using SnsMessage =  Amazon.SimpleNotificationService.Util.Message;
+
+namespace GasMon
+{
+    public class MessageParser
+    {
+        public Reading ParseMessage(Message message)
+        {
+            //come in later and write a test
+            var snsMessage = SnsMessage.ParseMessage(message.Body);
+            return JsonConvert.DeserializeObject<Reading>(snsMessage.MessageText);
+        }
+    }
+}

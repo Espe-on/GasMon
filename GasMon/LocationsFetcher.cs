e27f7928df36777dfe92ec42a3f09e121b40f3f7
f <fdf>
2020-04-01 12:41:49 +0100

    no longer calling env variable, instead using .aws/config file

--- GasMon/LocationsFetcher.cs
+++ GasMon/LocationsFetcher.cs
@@ -16,9 +16,7 @@ namespace GasMon
 
     public class LocationsFetcher : ILocationsFetcher
     {
-        private static readonly string AccessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY");
-        private static readonly string SecretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY");
-        private static readonly string BucketName = Environment.GetEnvironmentVariable("AWS_S3_BUCKET_NAME");
+        private static readonly string BucketName = "gasmonitoring-locationss3bucket-pgef0qqmgwba";
         private const string KeyName = "locations.json";
         private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest2;
         private static IAmazonS3 _client;
@@ -39,7 +37,7 @@ namespace GasMon
 
         public List<Location> LocationListMaker()
         {
-            _client = new AmazonS3Client(AccessKey, SecretKey, BucketRegion);
+            _client = new AmazonS3Client(BucketRegion);
             var returnedData = ReadObjectDataAsync(_client).Result;
             return JsonConvert.DeserializeObject<List<Location>>(returnedData);
         }

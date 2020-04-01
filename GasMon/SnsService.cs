a7ae9fd413d6f6973128d5dac9c50c70538233c5
f <fdf>
2020-04-01 11:12:11 +0100

    created sns service

---
+++ GasMon/SnsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+
+namespace GasMon
+{
+    public class SnsService
+    {
+        private static readonly string TopicArn = Environment.GetEnvironmentVariable("AWS_SNS_TOPIC_ARN");
+        private readonly IAmazonSimpleNotificationService _snsClient;
+        private readonly IAmazonSQS _sqsClient;
+
+        public SnsService(IAmazonSimpleNotificationService snsClient, IAmazonSQS sqsClient)
+        {
+            _snsClient = snsClient;
+            _sqsClient = sqsClient;
+        }
+
+        public async Task<string> SubscribeQueueAsync(string queueUrl)
+        {
+            return await _snsClient.SubscribeQueueAsync(TopicArn, _sqsClient, queueUrl);
+        }
+
+        public async Task UnsubscribeQueueAsync(string subscriptionArn)
+        {
+            await _snsClient.UnsubscribeAsync(subscriptionArn);
+        }
+    }
+}

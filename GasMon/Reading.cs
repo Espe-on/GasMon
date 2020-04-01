48a9d5bd6fd3d7f76161c3ddee99073361b11e2f
f <fdf>
2020-04-01 15:48:02 +0100

    defined type for Reading with custom to string method

---
+++ GasMon/Reading.cs
@@ -0,0 +1,15 @@
+﻿namespace GasMon
+{
+    public class Reading
+    {
+        public string EventId { get; set; }
+        public string LocationId { get; set; }
+        public double Value { get; set; }
+        public long Timestamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"EventID: {EventId}, LocationID: {LocationId}, Value:{Value}, Timestamp: {Timestamp}";
+        }
+    }
+}

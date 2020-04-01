6508cae705b93bcd02f4b76a04dd1355c8fed49e
f <fdf>
2020-04-01 11:15:21 +0100

    created a message manager

---
+++ GasMon/MessageManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GasMon
+{
+    public class MessageManager
+    {
+        private readonly SqsService _sqsService;
+
+        public MessageManager(SqsService sqsService)
+        {
+            _sqsService = sqsService;
+        }
+
+        public void ProcessMessages(string queueUrl)
+        {
+            var messages = _sqsService.FetchMessagesAsync(queueUrl).Result;
+            foreach (var message in messages)
+            {
+                Console.WriteLine(message.MessageId);
+                _sqsService.DeleteMessageAsync(queueUrl, message.ReceiptHandle).Wait();
+            }
+        }
+    }
+}
